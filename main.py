43c4bb7f18e8923f76dd7631890b22b8cb858599
Lincoln Sadler <lincolndiscord17@gmail.com>
2025-04-22 23:39:21 -0400

    translate input prompts to English

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-num1 = int(input('Digite um número '))
-num2 = int(input('Digite outro numero'))
+num1 = int(input('First number:'))
+num2 = int(input('Second number:'))
 operation = input('a operação [ + / - / * / ]')
 
 def calculator (num1, num2, operation):
