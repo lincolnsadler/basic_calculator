41e26f756d78d6ce1db5bdd868de32faf96363f0
Lincoln Sadler <lincolndiscord17@gmail.com>
2025-05-14 21:25:46 -0400

    refactor: move input and calculator function inside while loop for continuous operation

--- main.py
+++ main.py
@@ -1,23 +1,24 @@
-num1 = int(input('First number:'))
-num2 = int(input('Second number:'))
-operation = input('the operation: [ + / - / * / ]')
+while True:
+    num1 = int(input('First number:'))
+    num2 = int(input('Second number:'))
+    operation = input('the operation: [ + / - / * / ]')
 
-def calculator (num1, num2, operation):
-    if operation == '+':
-        return(num1 + num2)
-    elif operation == '-':
-        return(num1 - num2)
-    elif operation == '*':
-        return(num1 * num2)
-    elif operation == '/':
-        if num2 == 0:
-            print('Error')
-        return(num1 / num2)
-    else:
-        print('Invalid operation')
+    def calculator (num1, num2, operation):
+        if operation == '+':
+            return(num1 + num2)
+        elif operation == '-':
+            return(num1 - num2)
+        elif operation == '*':
+            return(num1 * num2)
+        elif operation == '/':
+            if num2 == 0:
+                print('Error')
+            return(num1 / num2)
+        else:
+            print('Invalid operation')
 
-result = calculator(num1, num2, operation)
-print(f'The value is: {result}')
 
-# This is just a simple project to increase my Github profile...
+    result = calculator(num1, num2, operation)
+    print(f'The value is: {result}')
+
 
